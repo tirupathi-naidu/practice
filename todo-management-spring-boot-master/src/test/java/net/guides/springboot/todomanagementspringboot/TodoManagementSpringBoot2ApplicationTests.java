51f9d02d301ce3bce98a369509fde952ea378180
Naresh Ramisetty <naresh.venkanna@gmail.com>
2022-11-12 22:07:47 +0000

    Commit-property file

--- todo-management-spring-boot-master/src/test/java/net/guides/springboot/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
+++ todo-management-spring-boot-master/src/test/java/net/guides/springboot/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
@@ -1,11 +1,11 @@
-package net.guides.todo.todomanagementspringboot2;
+package net.guides.springboot.todomanagementspringboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
 @SpringBootTest
 public class TodoManagementSpringBoot2ApplicationTests {
 
